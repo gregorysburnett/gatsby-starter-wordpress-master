c4e094ad46b3de8106a6f2934f4a4848cb1d96f6
gregorysburnett <greg@midwest-tropical.com>
2020-09-29 08:40:34 -0500

    update

--- src/gatsby-config.js
+++ src/gatsby-config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  plugins: [`gatsby-plugin-catch-links`],
+  plugins: [`gatsby-plugin-catch-links`], resolve: "gatsby-source-wordpress",
+      options: {
+        baseUrl: `www.midwest-tropical.com/GatsbyWP`,
+        protocol: `https`,
+        hostingWPCOM: false,
+        useACF: true,
 }
